5d6756307314ddf0394d0e57f7517905b3263db4
Aggelos Zaxariou <aggelos2ker@gmail.com>
2026-05-02 23:45:46 +0300

    Restore project placeholders

--- src/data/site.js
+++ src/data/site.js
@@ -72,6 +72,10 @@ export const site = {
         href: "https://fuzhou-garden.vercel.app/",
         icon: "/public/images/fuzhou-garden-logo.jpg",
       },
+      { title: "Soon!", href: "#", icon: "/public/images/AG.png" },
+      { title: "Soon!", href: "#", icon: "/public/images/AG.png" },
+      { title: "Soon!", href: "#", icon: "/public/images/AG.png" },
+      { title: "Soon!", href: "#", icon: "/public/images/AG.png" },
     ],
   },
   certifications: {
